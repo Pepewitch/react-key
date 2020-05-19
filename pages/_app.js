b1dac5aa70880a019bc215d247ed2eeb7a3e4fca
Witchayut <skpepe134@gmail.com>
2020-05-20 03:11:03 +0700

    add font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import "../tailwind.css";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Component {...pageProps}>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <style global jsx>{`
+          html {
+            font-family: "Jost", sans-serif;
+          }
+        `}</style>
+      </Head>
+    </Component>
+  );
 };
 
 export default App;
